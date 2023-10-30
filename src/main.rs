cae9578520a9371bace0cd78703da68fb76cc4bb
Magal-W <magalking8@gmail.com>
2023-10-31 00:25:44 +0200

    Add hello world using axum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 mod hebrew_db;
 
+use axum::{response::IntoResponse, Json};
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
 use hebrew_db::HebrewDb;
 use rusqlite::Result;
+use tokio;
 
-fn main() -> Result<()> {
-    let data = HebrewDb::new()?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/mistakes", get(get_mistakes));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
     Ok(())
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn get_mistakes() -> impl IntoResponse {
+    Json(HebrewDb::new().unwrap().mistaken_words().unwrap())
 }
